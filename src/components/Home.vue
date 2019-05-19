9aefb2c1a90a680c79d3d65ac8b611587796d18c
rexitorg <rexit.org@gmail.com>
2019-05-19 15:39:40 +0900

    add button alert

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        <MyButton greet="Konchan!!!"></MyButton>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+    import MyButton from '@/components/MyButton.vue';
+
+    @Component({
+        components: {
+            MyButton,
+        },
+    })
+
+    export default class Home extends Vue {
+    }
+</script>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="onClick">MyButton</button>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class MyButton extends Vue {
+        @Prop()
+        public greet?: string;
+
+        public onClick() {
+            alert(this.greet);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
